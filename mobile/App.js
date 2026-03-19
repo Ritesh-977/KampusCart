a4e894e4c85787479ab4d61956fbb117346782ee
Ritesh Singh <123322562+Ritesh-977@users.noreply.github.com>
2026-03-20 00:08:59 +0530

    Top notch black color

--- mobile/App.js
+++ mobile/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import { AuthProvider } from './src/context/AuthContext';
@@ -8,6 +9,7 @@ export default function App() {
   return (
     <AuthProvider>
       <SafeAreaProvider>
+        <StatusBar backgroundColor="#000000" barStyle="light-content" />
         <NavigationContainer>
           <AppNavigator />
         </NavigationContainer>
