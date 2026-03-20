613d613d1cbcf4a790cba10480a2f506a60ab696
Ritesh Singh <123322562+Ritesh-977@users.noreply.github.com>
2026-03-20 23:18:03 +0530

    online status

--- mobile/App.js
+++ mobile/App.js
@@ -3,17 +3,21 @@ import { StatusBar } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import { AuthProvider } from './src/context/AuthContext';
+import { SocketProvider } from './src/context/SocketContext';
 import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
   return (
     <AuthProvider>
-      <SafeAreaProvider>
-        <StatusBar backgroundColor="#000000" barStyle="light-content" />
-        <NavigationContainer>
-          <AppNavigator />
-        </NavigationContainer>
-      </SafeAreaProvider>
+      {/* SocketProvider is inside AuthProvider so it can read currentUser/userToken */}
+      <SocketProvider>
+        <SafeAreaProvider>
+          <StatusBar backgroundColor="#000000" barStyle="light-content" />
+          <NavigationContainer>
+            <AppNavigator />
+          </NavigationContainer>
+        </SafeAreaProvider>
+      </SocketProvider>
     </AuthProvider>
   );
 }
