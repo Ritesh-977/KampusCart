fb28c44142c5c81f9e0f2d9dd7fb5e4ae62b9dca
Ritesh Singh <123322562+Ritesh-977@users.noreply.github.com>
2026-03-21 03:53:40 +0530

    ..

--- mobile/App.js
+++ mobile/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
+import { KeyboardProvider } from 'react-native-keyboard-controller';
 import { AuthProvider } from './src/context/AuthContext';
 import { SocketProvider } from './src/context/SocketContext';
 import AppNavigator from './src/navigation/AppNavigator';
@@ -9,13 +10,14 @@ import AppNavigator from './src/navigation/AppNavigator';
 export default function App() {
   return (
     <AuthProvider>
-      {/* SocketProvider is inside AuthProvider so it can read currentUser/userToken */}
       <SocketProvider>
         <SafeAreaProvider>
-          <StatusBar backgroundColor="#000000" barStyle="light-content" />
-          <NavigationContainer>
-            <AppNavigator />
-          </NavigationContainer>
+          <KeyboardProvider>
+            <StatusBar backgroundColor="#000000" barStyle="light-content" />
+            <NavigationContainer>
+              <AppNavigator />
+            </NavigationContainer>
+          </KeyboardProvider>
         </SafeAreaProvider>
       </SocketProvider>
     </AuthProvider>
